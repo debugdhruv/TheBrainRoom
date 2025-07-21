7cdeeab7e352db0282c1c084586dc6121e15f985
Dhruv Narayan Tiwari <dhruvtiwari.1130@gmail.com>
2025-07-22 03:16:28 +0530

    Implemented OTP Auth Verification + Removed the unused codes and coments

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,83 +1,3 @@
-// const User = require("../models/User");
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
-
-// const generateToken = (userId) => {
-//   return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
-// };
-
-// exports.registerUser = async (req, res) => {
-//   const { firstName, lastName, email, password, gender, dob } = req.body;
-//   console.log("📥 Registration Payload:", req.body);
-
-//   try {
-//     const userExists = await User.findOne({ email });
-//     if (userExists) return res.status(400).json({ message: "Email already registered" });
-
-//     const salt = await bcrypt.genSalt(10);
-//     const hashedPassword = await bcrypt.hash(password, salt);
-
-//     const user = await User.create({
-//       firstName,
-//       lastName,
-//       email,
-//       password: hashedPassword,
-//       gender: gender || null,
-//       dob: dob ? new Date(dob) : null
-//     });
-
-//     const token = generateToken(user._id);
-
-//     res.status(201).json({
-//       user: {
-//         id: user._id,
-//         firstName: user.firstName,
-//         lastName: user.lastName,
-//         email: user.email,
-//         gender: user.gender,
-//         dob: user.dob
-//       },
-//       token
-//     });
-//   } catch (err) {
-//     console.error("❌ Register Error:", err);
-//     res.status(500).json({ message: err.message });
-//   }
-// };
-
-// exports.loginUser = async (req, res) => {
-//   const { email, password } = req.body;
-
-//   try {
-//     const user = await User.findOne({ email });
-//     if (!user) return res.status(400).json({ message: "Invalid credentials" });
-
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
-
-//     const token = generateToken(user._id);
-
-//     res.status(200).json({
-//       user: {
-//         id: user._id,
-//         firstName: user.firstName,
-//         lastName: user.lastName,
-//         email: user.email,
-//         gender: user.gender,
-//         dob: user.dob,
-//         xp: user.xp,
-//         xpHistory: user.xpHistory,
-//       },
-//       token
-//     });
-//   } catch (err) {
-//     console.error("❌ Login Error:", err);
-//     res.status(500).json({ message: err.message });
-//   }
-// };
-
-
-
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -91,10 +11,7 @@ const registerUser = async (req, res) => {
   const { firstName, lastName, email, password, gender, dob } = req.body;
   console.log("📥 Registration Payload:", req.body);
 
-  // OTP verification
-  // const { otp } = req.body;
-  // // const isOtpValid = otpService.verifyOTP(email, otp);
-  // if (!isOtpValid) return res.status(400).json({ message: "Invalid or expired OTP" });
+
 
   try {
     const userExists = await User.findOne({ email });
